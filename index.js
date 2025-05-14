c564cd6f7b28ec9710570b9bdea2c3c2c867372b
Darksi24 <yr84865@gmail.com>
2025-05-13 18:28:38 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
-const express = require("express");
+// index.js
+import express from 'express';
+import { Telegraf } from 'telegraf';
+
 const app = express();
+const bot = new Telegraf('7442347920:AAG7dHEtlVrqHEq-SJaPo8qsWilJbmsk7aM');
 
-// Inicia el bot
-require("./telegram"); // Esto lanza el bot
-
-// Ruta web simple
-app.get("/", (req, res) => {
-  res.send("Bot activo y funcionando.");
+app.use(express.json());
+app.post(`/webhook`, (req, res) => {
+  bot.handleUpdate(req.body, res);
 });
 
-// Inicia servidor web
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Servidor web corriendo en puerto ${PORT}`);
+bot.telegram.setWebhook('https://naori-best.vercel.app/webhook');
+
+app.get('/', (req, res) => {
+  res.send('Bot activo y funcionando');
 });
+
+export default app;
