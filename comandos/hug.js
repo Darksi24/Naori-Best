4b79488f18a26a2f307f5d184deaa84218ee5ad4
Darksi24 <yr84865@gmail.com>
2025-05-14 00:25:37 -0600

    Hug

--- comandos/hug.js
+++ comandos/hug.js
@@ -2,29 +2,32 @@ const gifs = ["gifs/hug.gif", "gifs/hug2.gif", "gifs/hug3.gif", "gifs/hug4.gif",
 
 
 module.exports = {
-  name: "hug",
-  execute: async (ctx) => {
-    const autor = ctx.from.first_name;
-    const reply = ctx.message.reply_to_message;
-    const destinatario = reply ? reply.from.first_name : null;
-    const userId = ctx.from.id;
+    name: "hug",
+    execute: async (ctx) => {
+        const autor = ctx.from.first_name;
+        const reply = ctx.message.reply_to_message;
+        const destinatario = reply ? reply.from.first_name: null;
+        const userId = ctx.from.id;
+
+        const randomGif = gifs[Math.floor(Math.random() * gifs.length)];
 
 
-	if (!destinatario) {
-	  return ctx.reply("Debes responder a un mensaje de alguien.");
-	}}
+        if (!destinatario) {
+            return ctx.reply("Debes responder a un mensaje de alguien.");
+        }}
 
 
-	const randomGif = gifs[Math.floor(Math.random() * gifs.length)];
 
-	await ctx.replyWithAnimation(
-		{ source: randomGif },
-		{
-			caption: `*${autor}* le dio un abrazito a *${destinatario}*`,
-     		parse_mode: "Markdown"
-		}
-	);
-	
-    
-  }
+    await ctx.replyWithAnimation(
+        {
+            source: randomGif
+        },
+        {
+            caption: `*${autor}* le dio un abrazito a *${destinatario}*`,
+            parse_mode: "Markdown"
+        }
+    );
+
+
+}
 }
