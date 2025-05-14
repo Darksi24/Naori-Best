5985f924b5ae9c05485578d3a3c718ebc735839c
Darksi24 <yr84865@gmail.com>
2025-05-13 19:48:13 -0600

    Update webhook.js

--- api/webhook.js
+++ api/webhook.js
@@ -1,16 +1,15 @@
-// api/webhook.js
-const bot = require("../../telegram");
+import telegramBot from "../telegram.js";
 
 export default async function handler(req, res) {
-  if (req.method === "POST") {
-    try {
-      await bot.handleUpdate(req.body);
-      res.status(200).send("ok");
-    } catch (err) {
-      console.error("Error handling update", err);
-      res.status(500).send("Error");
-    }
-  } else {
-    res.status(200).send("Webhook works!");
+  if (req.method !== "POST") {
+    return res.status(405).send("Use POST");
+  }
+  try {
+    // Telegram envía JSON con la actualización
+    await telegramBot.handleUpdate(req.body);
+    return res.status(200).send("OK");
+  } catch (err) {
+    console.error("Error en webhook:", err);
+    return res.status(500).send("Error interno");
   }
 }
